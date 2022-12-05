6df82d17f4b2a3986d010f4abddd0261bae8536c
gibran abdillah <70421698+gibran-abdillah@users.noreply.github.com>
2022-12-05 16:47:28 +0700

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8%(9o6ed=abewj#ex=+ov9afyn_6s9)wy8+_9vi1k&!97_%ny1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', default=False)
+DEBUG = os.environ.get('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','.herokuapp.com','.vercel.app']
 
 
 # Application definition
@@ -86,10 +86,10 @@ LOGIN_REDIRECT_URL = '/account'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 if not DEBUG:
